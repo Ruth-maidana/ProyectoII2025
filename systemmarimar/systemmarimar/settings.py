08ee2a4ab30fdf5050dda9d6b66d9fbdad39302d
ruth <rutieli25@gmail.com>
2025-09-14 23:56:31 -0300

    Cambios 3 en el settings

--- systemmarimar/systemmarimar/settings.py
+++ systemmarimar/systemmarimar/settings.py
@@ -50,7 +50,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = os.environ.get("SECRET_KEY","dev-secret-key")  # definido en Railway
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
